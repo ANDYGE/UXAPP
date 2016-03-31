fe6d2f4d452ca68d4f4aaf647ecdb6f887183ccf
andyge <geshaofei@126.com>
2016-03-31 08:39:40 +0800

    Initial Commit

---
+++ UXAPP/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UXAPP
+//
+//  Created by 葛绍飞 on 16/3/31.
+//  Copyright © 2016年 sfeig. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UXAPP/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UXAPP
+//
+//  Created by 葛绍飞 on 16/3/31.
+//  Copyright © 2016年 sfeig. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
